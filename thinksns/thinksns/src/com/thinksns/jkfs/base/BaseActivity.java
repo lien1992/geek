0d170836fd7c7ac16bd1961207a49e8fa9caa549
moshenglei <2645077926@qq.com>
2014-05-18 18:41:09 +0800

    fix

--- thinksns/thinksns/src/com/thinksns/jkfs/base/BaseActivity.java
+++ thinksns/thinksns/src/com/thinksns/jkfs/base/BaseActivity.java
@@ -1,11 +1,12 @@
 package com.thinksns.jkfs.base;
 
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by mosl on 14-5-11.
  */
-public class BaseActivity extends Activity {
+public class BaseActivity extends FragmentActivity{
 
 
 }
