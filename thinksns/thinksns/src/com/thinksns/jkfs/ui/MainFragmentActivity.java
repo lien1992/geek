01855ad788f7aa60eb7ae9996a5a36ea59603896
moshenglei <2645077926@qq.com>
2014-05-19 22:27:47 +0800

    fix

--- thinksns/thinksns/src/com/thinksns/jkfs/ui/MainFragmentActivity.java
+++ thinksns/thinksns/src/com/thinksns/jkfs/ui/MainFragmentActivity.java
@@ -10,11 +10,21 @@ import com.thinksns.jkfs.base.BaseSlidingFragmentActivity;
 import com.thinksns.jkfs.base.ThinkSNSApplication;
 import com.thinksns.jkfs.bean.AccountBean;
 import com.thinksns.jkfs.ui.adapter.MainFragmentPagerAdapter;
+import com.thinksns.jkfs.ui.fragment.AboutMeFragment;
+import com.thinksns.jkfs.ui.fragment.ChanelFragment;
+import com.thinksns.jkfs.ui.fragment.ChatFragment;
+import com.thinksns.jkfs.ui.fragment.CollectionFragment;
+import com.thinksns.jkfs.ui.fragment.HomeFragment;
+import com.thinksns.jkfs.ui.fragment.WeibaFragment;
 import com.thinksns.jkfs.ui.view.SlidingMenu;
 
+import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -59,15 +69,50 @@ public class MainFragmentActivity extends BaseSlidingFragmentActivity implements
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 
-				// 单击侧滑菜单中的某一项
+			switch (position){
+                case 0:
+                    HomeFragment home=new HomeFragment();
+                    setFragment(home);
+                    break;
+                case 1:
+                    ChatFragment chat=new ChatFragment();
+                    setFragment(chat);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    CollectionFragment collection =new CollectionFragment();
+                    setFragment(collection);
+                    break;
+                case 4:
+                    AboutMeFragment aboutMe=new AboutMeFragment();
+                    setFragment(aboutMe);
+                    break;
+                case 5:
+                    ChanelFragment chanel=new ChanelFragment();
+                    setFragment(chanel);
+                    break;
+                case 6:
+                    WeibaFragment weiba=new WeibaFragment();
+                    setFragment(weiba);
+                    break;
+
+            }
 
 			}
 		});
 
 	}
 
+    private void setFragment(Fragment fragment){
+        if(fragment==null)
+            return;
+        FragmentManager f=getSupportFragmentManager();
+        FragmentTransaction ft=f.beginTransaction();
+        ft.replace(R.id.above_pager,fragment);
+        ft.commit();
+    }
 	private void initSlidingMenu() {
-		// TODO Auto-generated method stub
 		setBehindContentView(R.layout.slidingmenu_behind);
 		sm = getSlidingMenu();
 		sm.setShadowWidthRes(R.dimen.shadow_width);
--- thinksns/thinksns/src/com/thinksns/jkfs/ui/fragment/ChatFragment.java
+++ thinksns/thinksns/src/com/thinksns/jkfs/ui/fragment/ChatFragment.java
@@ -6,10 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.thinksns.jkfs.R;
+
 public class ChatFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.chat_fragment_layout,container,false);
     }
 }
