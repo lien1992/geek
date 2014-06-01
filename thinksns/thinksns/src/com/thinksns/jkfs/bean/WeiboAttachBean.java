4a40b46c1fb7e8237a9670b43ca8cccdd3e393e3
win258 <win258@126.com>
2014-06-01 23:10:51 +0800

    fix

--- thinksns/thinksns/src/com/thinksns/jkfs/bean/WeiboAttachBean.java
+++ thinksns/thinksns/src/com/thinksns/jkfs/bean/WeiboAttachBean.java
@@ -5,15 +5,14 @@ import android.os.Parcelable;
 
 /**
  * 微博附件
- *
+ * 
  */
-public class WeiboAttachBean implements Parcelable{
+public class WeiboAttachBean implements Parcelable {
 	private String attach_id;
 	private String attach_name;
 	private String attach_url;
 	private String attach_small;
 	private String attach_middle;
-	
 
 	public String getId() {
 		return attach_id;
@@ -46,10 +45,37 @@ public class WeiboAttachBean implements Parcelable{
 	}
 
 	@Override
-	public void writeToParcel(Parcel arg0, int arg1) {
+	public void writeToParcel(Parcel dest, int arg1) {
 		// TODO Auto-generated method stub
-		
+		dest.writeString(attach_id);
+		dest.writeString(attach_name);
+		dest.writeString(attach_url);
+		dest.writeString(attach_small);
+		dest.writeString(attach_middle);
 	}
+
+	public static final Parcelable.Creator<WeiboAttachBean> CREATOR = new Parcelable.Creator<WeiboAttachBean>() {
+
+		@Override
+		public WeiboAttachBean createFromParcel(Parcel in) {
+			// TODO Auto-generated method stub
+			WeiboAttachBean wab = new WeiboAttachBean();
+			wab.attach_id = in.readString();
+			wab.attach_name = in.readString();
+			wab.attach_url = in.readString();
+			wab.attach_small = in.readString();
+			wab.attach_middle = in.readString();
+
+			return wab;
+		}
+
+		@Override
+		public WeiboAttachBean[] newArray(int size) {
+			// TODO Auto-generated method stub
+			return new WeiboAttachBean[size];
+		}
+
+	};
 
 	public void setAttach_small(String attach_small) {
 		this.attach_small = attach_small;
@@ -66,6 +92,5 @@ public class WeiboAttachBean implements Parcelable{
 	public String getAttach_middle() {
 		return attach_middle;
 	}
-
 
 }
