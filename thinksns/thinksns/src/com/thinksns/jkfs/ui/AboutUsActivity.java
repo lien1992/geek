908687c84b672c6fd6a2ac89405440abb0cdd76a
win258 <win258@126.com>
2014-06-04 22:02:14 +0800

    fix

--- thinksns/thinksns/src/com/thinksns/jkfs/ui/AboutUsActivity.java
+++ thinksns/thinksns/src/com/thinksns/jkfs/ui/AboutUsActivity.java
@@ -1,7 +1,31 @@
 package com.thinksns.jkfs.ui;
 
-import android.app.Activity;
+import com.thinksns.jkfs.R;
 
-public class AboutUsActivity extends Activity{
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class AboutUsActivity extends Activity {
+	private ImageView back;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_aboutus);
+		back = (ImageView) findViewById(R.id.aboutus_back);
+		back.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+
+		});
+
+	}
 
 }
