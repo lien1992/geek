f2d0f4aba4f1ca6008c5c2241c661102a4e022e3
win258 <win258@126.com>
2014-08-18 00:39:34 +0800

    修复少许bug，图片支持手势放缩

--- thinksns/thinksns/src/com/thinksns/jkfs/ui/BrowseImageActivity.java
+++ thinksns/thinksns/src/com/thinksns/jkfs/ui/BrowseImageActivity.java
@@ -2,34 +2,40 @@ package com.thinksns.jkfs.ui;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageSize;
 import com.thinksns.jkfs.R;
+import com.thinksns.jkfs.ui.view.DragImageView;
 import com.thinksns.jkfs.ui.view.ImageLoadingDialog;
 import com.thinksns.jkfs.util.Utility;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.view.GestureDetector.OnGestureListener;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class BrowseImageActivity extends Activity implements OnGestureListener {
+public class BrowseImageActivity extends Activity {
 	private String url;
-	private ImageView image;
+	private DragImageView image;
 	private ImageLoadingDialog dialog;
 	private DisplayImageOptions options;
-
-	private GestureDetector detector;
 	private Bitmap bitmap;
-	private int width, height;
-	private float currentScale = 1;
-	private Matrix matrix;
+
+	private int window_width, window_height;
+	private int state_height;// 状态栏的高度
+
+	private ViewTreeObserver viewTreeObserver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,23 +47,43 @@ public class BrowseImageActivity extends Activity implements OnGestureListener {
 		url = intent.getStringExtra("url");
 		Log.d("wj", "browse image url:" + url);
 
+		WindowManager manager = getWindowManager();
+		window_width = manager.getDefaultDisplay().getWidth();
+		window_height = manager.getDefaultDisplay().getHeight();
+
 		options = new DisplayImageOptions.Builder().showStubImage(
 				R.drawable.ic_launcher).cacheInMemory().cacheOnDisc().build();
 
-		image = (ImageView) findViewById(R.id.browse_big_image);
+		image = (DragImageView) findViewById(R.id.browse_big_image);
 		dialog = new ImageLoadingDialog(this);
 		dialog.setCanceledOnTouchOutside(false);
 		dialog.show();
 
-		detector = new GestureDetector(this);
-		matrix = new Matrix();
-
 		if (Utility.isConnected(this)) {
-			ImageLoader.getInstance().displayImage(url, image, options);
 			dialog.dismiss();
-			bitmap = ImageLoader.getInstance().loadImageSync(url);
-			width = bitmap.getWidth();
-			height = bitmap.getHeight();
+			ImageSize targetSize = new ImageSize(window_width, window_height);
+			bitmap = ImageLoader.getInstance().loadImageSync(url, targetSize,
+					options);
+			image.setImageBitmap(bitmap);
+			image.setmActivity(this);
+			viewTreeObserver = image.getViewTreeObserver();
+			viewTreeObserver
+					.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+
+						@Override
+						public void onGlobalLayout() {
+							if (state_height == 0) {
+								// 获取状况栏高度
+								Rect frame = new Rect();
+								getWindow().getDecorView()
+										.getWindowVisibleDisplayFrame(frame);
+								state_height = frame.top;
+								image.setScreen_H(window_height - state_height);
+								image.setScreen_W(window_width);
+							}
+
+						}
+					});
 		} else {
 			dialog.dismiss();
 			Toast.makeText(BrowseImageActivity.this, "网络未连接",
@@ -69,69 +95,6 @@ public class BrowseImageActivity extends Activity implements OnGestureListener {
 				}
 			}, 1000 * 2);
 		}
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		// 将该Activity上的触碰事件交给GestureDetector处理
-		return detector.onTouchEvent(event);
-	}
-
-	@Override
-	public boolean onDown(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-			float velocityY) {
-		// TODO Auto-generated method stub
-		velocityX = velocityX > 4000 ? 4000 : velocityX;
-		velocityX = velocityX < -4000 ? -4000 : velocityX;
-		// 根据手势的速度来计算缩放比，如果velocityX>0，放大图像，否则缩小图像
-		currentScale += currentScale * velocityX / 4000.0f;
-		// 保证currentScale不会等于0
-		currentScale = currentScale > 0.01 ? currentScale : 0.01f;
-		// 重置Matrix
-		matrix.reset();
-		// 缩放Matrix
-		matrix.setScale(currentScale, currentScale, 160, 200);
-/*		// 如果图片还未回收，先强制回收该图片
-		if (!bitmap.isRecycled()) {
-			bitmap.recycle();
-		}*/
-		// 根据原始位图和Matrix创建新图片
-		Bitmap bitmap2 = Bitmap.createBitmap(bitmap, 0, 0, width, height,
-				matrix, true);
-		// 显示新的位图
-		image.setImageBitmap(bitmap2);
-		return false;
-	}
-
-	@Override
-	public void onLongPress(MotionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-			float distanceY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void onShowPress(MotionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean onSingleTapUp(MotionEvent e) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
--- thinksns/thinksns/src/com/thinksns/jkfs/ui/adapter/CommentAdapter.java
+++ thinksns/thinksns/src/com/thinksns/jkfs/ui/adapter/CommentAdapter.java
@@ -1,19 +1,31 @@
 package com.thinksns.jkfs.ui.adapter;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.google.gson.Gson;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.thinksns.jkfs.R;
+import com.thinksns.jkfs.base.ThinkSNSApplication;
 import com.thinksns.jkfs.bean.CommentBean;
+import com.thinksns.jkfs.bean.UserInfoBean;
+import com.thinksns.jkfs.constant.HttpConstant;
+import com.thinksns.jkfs.ui.OtherInfoActivity;
+import com.thinksns.jkfs.ui.WeiboDetailActivity;
 import com.thinksns.jkfs.ui.view.RoundAngleImageView;
+import com.thinksns.jkfs.util.Utility;
+import com.thinksns.jkfs.util.http.HttpMethod;
+import com.thinksns.jkfs.util.http.HttpUtility;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -85,12 +97,11 @@ public class CommentAdapter extends BaseAdapter {
 		// TODO Auto-generated method stub
 		final ViewHolder holder;
 		final CommentBean comment = cList.get(position);
-		Log.d("comment adapter item is null?", (comment == null) + "");
+		Log.d("wj", "comment adapter item is null?" + (comment == null));
 		if (convertView == null) {
 			holder = new ViewHolder();
 			convertView = in.inflate(R.layout.main_weibo_comment_listview_item,
 					null);
-			Log.d("in is null ?", (in == null) + "");
 			holder.avatar = (RoundAngleImageView) convertView
 					.findViewById(R.id.wb_cmt_user_img);
 			holder.userName = (TextView) convertView
@@ -108,7 +119,41 @@ public class CommentAdapter extends BaseAdapter {
 		ImageLoader.getInstance().displayImage(
 				comment.getUser_info().getAvatar_small(), holder.avatar,
 				options);
+		holder.avatar.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (comment != null) {
+					Intent in_o = new Intent(ctx, OtherInfoActivity.class);
+					in_o.putExtra("userinfo", comment.getUid());
+					Log.d("wj", "comment.getUser_info().follow_state is null?"
+							+ (comment.getUser_info().follow_state == null));// true
+					in_o.putExtra("following",
+							comment.getUser_info().follow_state.getFollowing()
+									+ "");
+					ctx.startActivity(in_o);
+				}
+			}
+
+		});
 		holder.userName.setText(comment.getUser_info().getUname());
+		holder.userName.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (comment != null) {
+					Intent in_o = new Intent(ctx, OtherInfoActivity.class);
+					in_o.putExtra("userinfo", comment.getUid());
+					in_o.putExtra("following",
+							comment.getUser_info().follow_state.getFollowing()
+									+ "");
+					ctx.startActivity(in_o);
+				}
+			}
+
+		});
 		holder.content.setText(comment.getListViewSpannableString());
 		holder.time.setText(comment.getCtime());
 
